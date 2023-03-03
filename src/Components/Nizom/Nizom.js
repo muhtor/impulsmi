58f6f57b70573006821bc5da2e7825c9dee52798
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2023-03-03 17:44:01 +0500

    .

--- src/Components/Nizom/Nizom.js
+++ src/Components/Nizom/Nizom.js
@@ -6,25 +6,30 @@ import { BsArrowUpRightSquareFill } from 'react-icons/bs';
 import nizom_img from '../../imgs/nizom img.jpg'
 
 
+import { useTranslation } from "react-i18next";
+
 function Nizom(){
+
+    const { t } = useTranslation();
+
     return(
         <>
             <div className="nizom_wrapper">
                 <div className="nizom_banner">
                      <div>
-                        <p><Link to="/">BOSH SAHIFA</Link> Nizom</p>
+                        <p><Link to="/">{t("bosh_sahifa")}</Link> {t("nizom_page_text")}</p>
                     </div>
-                    <h1>IMPULS TIBBIYOT INSTITUTI NIZOMI</h1>
+                    <h1>{t("nizom")}</h1>
                     <div className="nizom">
                         <div>
                             <div className="nizom_img">
                                 <img src={nizom_img} alt="" />
                             </div>
-                            <a href="#" >IMPULS TIBBIYOT INSTITUTI NIZOMI</a>
+                            <a href="#" className="desktop-nizom" >{t("nizom")}</a>
                         </div>
-                            <a href="#" className="nizom_text">IMPULS TIBBIYOT INSTITUTI NIZOMI</a>
-                        <a href="#" className="view_icon"><BsArrowUpRightSquareFill /></a>
-                        <a href="#" className="view_btn">View</a>
+                            <a href="#" className="nizom_text">{t("nizom")}</a>
+                            <a href="#" className="view_icon"><BsArrowUpRightSquareFill /></a>
+                            <a href="#" className="view_btn">{t("nizom-btn")}</a>
                     </div>
                 </div>
             </div>
