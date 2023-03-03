2ed26858c1a83579a53409b2dab4e052d2030c32
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2023-03-03 16:45:11 +0500

    Nizom

--- src/Components/Nizom/Nizom.js
+++ src/Components/Nizom/Nizom.js
@@ -1,36 +1,36 @@
-// import React from "react";
-// import {Link} from 'react-router-dom';
-// import './Nizom.css'
+import React from "react";
+import {Link} from 'react-router-dom';
+import './Nizom.css'
 
-// import { BsArrowUpRightSquareFill } from 'react-icons/bs';
-// import nizom_img from '../../imgs/nizom img.jpg'
-
-
-// function Nizom(){
-//     return(
-//         <>
-//             <div className="nizom_wrapper">
-//                 <div className="nizom_banner">
-//                      <div>
-//                         <p><Link to="/">BOSH SAHIFA</Link> Nizom</p>
-//                     </div>
-//                     <h1>IMPULS TIBBIYOT INSTITUTI NIZOMI</h1>
-//                     <div className="nizom">
-//                         <div>
-//                             <div className="nizom_img">
-//                                 <img src={nizom_img} alt="" />
-//                             </div>
-//                             <a href="#" >IMPULS TIBBIYOT INSTITUTI NIZOMI</a>
-//                         </div>
-//                             <a href="#" className="nizom_text">IMPULS TIBBIYOT INSTITUTI NIZOMI</a>
-//                         <a href="#" className="view_icon"><BsArrowUpRightSquareFill /></a>
-//                         <a href="#" className="view_btn">View</a>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
+import { BsArrowUpRightSquareFill } from 'react-icons/bs';
+import nizom_img from '../../imgs/nizom img.jpg'
 
 
-// export default Nizom;
+function Nizom(){
+    return(
+        <>
+            <div className="nizom_wrapper">
+                <div className="nizom_banner">
+                     <div>
+                        <p><Link to="/">BOSH SAHIFA</Link> Nizom</p>
+                    </div>
+                    <h1>IMPULS TIBBIYOT INSTITUTI NIZOMI</h1>
+                    <div className="nizom">
+                        <div>
+                            <div className="nizom_img">
+                                <img src={nizom_img} alt="" />
+                            </div>
+                            <a href="#" >IMPULS TIBBIYOT INSTITUTI NIZOMI</a>
+                        </div>
+                            <a href="#" className="nizom_text">IMPULS TIBBIYOT INSTITUTI NIZOMI</a>
+                        <a href="#" className="view_icon"><BsArrowUpRightSquareFill /></a>
+                        <a href="#" className="view_btn">View</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Nizom;
