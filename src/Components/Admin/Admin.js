35e1b0bac501f2a21b40a756cd3d61e154a23966
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2023-07-10 15:10:56 +0500

    change admin password

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -14,8 +14,8 @@ function Admin() {
     let myteast_list = document.querySelector('.mytest');
     let input_value = document.querySelector('.input_value');
 
-    // let password = '1921680103'
-    let password = '8888'
+    let password = '1921680103'
+    // let password = '8888'
     // console.log(value.target.value);
     if (input_value.value === password) {
       console.log('togri');
